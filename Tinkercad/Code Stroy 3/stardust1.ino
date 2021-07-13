e33e187c3f5e08888480b90e41db02b9abc36694
EWilletts17 <ewilletts.17@mountsbay.org>
2021-07-13 11:42:19 +0100

    Saving

--- Tinkercad/Code Stroy 3/stardust1.ino
+++ Tinkercad/Code Stroy 3/stardust1.ino
@@ -1,36 +1,52 @@
+const int FWDRIGHT = 12;
+const int REVRIGHT = 11;
+const int ENABLERIGHT = 10;
+const int ENABLELEFT = 9;
+const int FWDLEFT = 8; 
+const int REVLEFT = 7;
+
 void setup()
 {
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(7, OUTPUT);
+  pinMode(FWDRIGHT, OUTPUT);
+  pinMode(REVRIGHT, OUTPUT);
+  pinMode(ENABLERIGHT, OUTPUT);
+  pinMode(ENABLELEFT, OUTPUT);
+  pinMode(FWDLEFT, OUTPUT);
+  pinMode(REVLEFT, OUTPUT);
+}
+void loop(){
+  
+  forwards();			
+  delay(5000);
+  
+  stop();
+  delay(1000);
+  
+  reverse();
+  delay(2000);
+  
+  stop();
 }
 
-void loop()
-{
-  digitalWrite(12, HIGH);	//Pin 12 High
-  digitalWrite(10, HIGH);	//Pin 10 High
-  digitalWrite(9, HIGH);	//Pin 9 High
-  digitalWrite(8, HIGH);	//Pin 8 High
-  delay(5000);				//Delay of 5 secconds
-  digitalWrite(12, LOW);	//Pin 12 Low
-  digitalWrite(10, LOW);	//Pin 10 Low
-  digitalWrite(9, LOW);		//Pin 9 Low
-  digitalWrite(8, LOW);		//Pin 8 Low
-  delay(1000);				//Delay of 1 seccond
-  digitalWrite(10, HIGH);	//Pin 10 High
-  digitalWrite(9, HIGH);	//Pin 9 High
-  digitalWrite(11, HIGH);	//Pin 11 High
-  digitalWrite(7, HIGH);	//Pin 7 High
-  delay(2000);				//Delay of secconds
-  digitalWrite(10, LOW);	//Pin 10 Low
-  digitalWrite(9, LOW);		//Pin 9 Low
-  digitalWrite(11, LOW);	//Pin 11 Low
-  digitalWrite(7, LOW);		//Pin 7 Low
-  delay(2000);				//Delay of 2 secconds
-} 
+void forwards(){
+  digitalWrite(FWDRIGHT, HIGH);				
+  digitalWrite(ENABLERIGHT, HIGH);		
+  digitalWrite(ENABLELEFT, HIGH);			
+  digitalWrite(FWDLEFT, HIGH);
+}
 
+void reverse(){
+  digitalWrite(ENABLERIGHT, HIGH);
+  digitalWrite(ENABLELEFT, HIGH);			
+  digitalWrite(REVRIGHT, HIGH);				
+  digitalWrite(REVLEFT, HIGH);	
+}
 
-
+void stop(){
+  digitalWrite(FWDRIGHT, LOW);				
+  digitalWrite(ENABLERIGHT, LOW);			
+  digitalWrite(ENABLELEFT, LOW);			
+  digitalWrite(FWDLEFT, LOW);
+  digitalWrite(REVRIGHT, LOW);
+  digitalWrite(REVLEFT,LOW);
+}
